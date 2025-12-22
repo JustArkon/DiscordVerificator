4c6f487a92a0ff0019cce181a7320ea515b32b94
justarkon <justarkon@gmail.com>
2025-12-22 14:28:25 +0200

    refactor: rewrite config logic

---
+++ src/main/java/net/justempire/discordverificator/configuration/Configuration.java
@@ -0,0 +1,9 @@
+package net.justempire.discordverificator.configuration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Configuration extends MessageConfigurationBase {
+    public Configuration(JavaPlugin plugin) {
+        super(plugin, "messages");
+    }
+}
